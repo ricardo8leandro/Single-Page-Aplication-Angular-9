075c8a735d29a08b61caceb4eb32b1e967de9b0e
Ricardo Leandro <ricardo8leandro@gmail.com>
2020-07-08 19:16:08 -0300

    Projeto CRUD Single Page Aplication - Angular 9

---
+++ backend/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,44 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(private ProductService: ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+
+
+  }
+
+  createProduct(): void {
+
+      this.ProductService.create(this.product).subscribe(()=>{
+
+      this.ProductService.showMessage('Produto criado')
+    
+      this.router.navigate(['/products'])
+
+    })
+
+  }
+
+  cancel(): void {
+
+    this.router.navigate(['/products'])
+
+  }
+
+
+}
